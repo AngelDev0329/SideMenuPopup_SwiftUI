015f7cf6cd96a2fad55835027044167a6c570cdd
AngelDev727 <54489723+AngelDev727@users.noreply.github.com>
2021-12-10 16:17:57 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# SwiftUI-SideMenuPopup
+Example of side menu popup in SwiftUI
+
+![Example Gif](/SideMenu.gif)
+
+Credit: [ProgrammingWithSwift.com](https://programmingwithswift.com/create-side-menu-with-swiftui/)
---
+++ Side Menu Example/ContentView.swift
@@ -0,0 +1,85 @@
+//
+//  ContentView.swift
+//  Side Menu Example
+//
+//  Created by Josh Hubbard on 1/10/20.
+//  Copyright © 2020 YeetBox. All rights reserved.
+//
+
+import SwiftUI
+
+struct MenuContent: View {
+    var body: some View {
+        List {
+            Text("My Profile").onTapGesture {
+                print("My Profile")
+            }
+            Text("Posts").onTapGesture {
+                print("Posts")
+            }
+            Text("Logout").onTapGesture {
+                print("Logout")
+            }
+        }
+    }
+}
+
+struct SideMenu: View {
+    let width: CGFloat
+    let isOpen: Bool
+    let menuClose: () -> Void
+    
+    var body: some View {
+        ZStack {
+            GeometryReader { _ in
+                EmptyView()
+            }
+            .background(Color.gray.opacity(0.3))
+            .opacity(self.isOpen ? 1.0 : 0.0)
+            .animation(Animation.easeIn.delay(0.25))
+            .onTapGesture {
+                self.menuClose()
+            }
+            
+            HStack {
+                MenuContent()
+                    .frame(width: self.width)
+                    .background(Color.white)
+                    .offset(x: self.isOpen ? 0 : -self.width)
+                    .animation(.default)
+                
+                Spacer()
+            }
+        }
+    }
+}
+
+struct ContentView: View {
+    @State var menuOpen: Bool = false
+    
+    var body: some View {
+        ZStack {
+            if !self.menuOpen {
+                Button(action: {
+                    self.openMenu()
+                }, label: {
+                    Text("Open")
+                })
+            }
+            
+            SideMenu(width: 270,
+                     isOpen: self.menuOpen,
+                     menuClose: self.openMenu)
+        }
+    }
+    
+    func openMenu() {
+        self.menuOpen.toggle()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
